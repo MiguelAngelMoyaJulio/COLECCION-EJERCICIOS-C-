5a1fff237d72b348a8c92ff70dab516de1f9a5f3
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-16 20:01:02 -0300

    E1 ARITHMETIC OPERARTIONS

---
+++ EJERCICIOS/E1/E1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace E1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 0;
+            Console.WriteLine("ingrese dos numeros");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine(a + " + " + b + " : " + (a + b));
+            Console.WriteLine(a + " - " + b + " : " + (a - b));
+            Console.WriteLine(a + " * " + b + " : " + (a * b));
+        }
+    }
+}
