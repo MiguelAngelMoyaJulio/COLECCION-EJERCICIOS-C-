4b7347af0e4c25fcbf3cf65e48ff9f0da0a3bf58
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-16 20:40:14 -0300

    E4 DATE

---
+++ EJERCICIOS/E4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace E4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a=0, m=0, d=0;
+            Console.WriteLine("enter year month and day");
+            a = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            d = int.Parse(Console.ReadLine());
+            Console.WriteLine("date : "+(a*1000+m*100+d));
+            
+        }
+    }
+}
