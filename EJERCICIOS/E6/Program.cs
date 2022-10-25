ab66b9cdc3b8bce10b28204c2518591c004cac3a
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-25 15:28:31 -0300

    E6 N NATURAL NUMBERS

---
+++ EJERCICIOS/E6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace E6
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 0;
+            Console.WriteLine("cantidad de numeros naturales que desea ver");
+            n = int.Parse(Console.ReadLine());
+            if (n != 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("el valor ingresado no es válido");
+            }
+        }
+    }
+}
