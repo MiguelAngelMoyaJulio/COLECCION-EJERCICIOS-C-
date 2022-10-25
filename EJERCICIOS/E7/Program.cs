181ad165a9f31df0ea193e9dd56576688f017e7e
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-25 15:36:59 -0300

    E7 MULTIPLICATION BY SUCCESSIVE ADDITIONS

---
+++ EJERCICIOS/E7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace E7
+{
+    /**
+     * Dados dos valores numéricos enteros, calcular e
+     * informar su producto mediante sumas sucesivas
+     */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n=0, m = 0, total=0;
+            Console.WriteLine("ingrese dos numeros");
+            n = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            for (int i = 1; i <=m ; i++)
+            {
+                total = total + n;
+            }
+
+            Console.WriteLine("multiplicacion : "+total);
+        }
+    }
+}
