d4b846b0142322b7b212b9a34844b85fe2929112
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-26 23:02:16 -0300

    E9 M MULTIPLES OF 3 BUT NOT 5

---
+++ EJERCICIOS/E9/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace E9
+{
+    /**
+ * Dado un valor numérico entero m, determinar e imprimir un listado
+ * con los m primeros múltiplos de 3 que no sean múltiples de 5.
+ */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int m = 0, i = 1, tot=0;
+            Console.WriteLine("ENTER A NUMBER");
+            m = int.Parse(Console.ReadLine());
+            while (tot < m)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine(i);
+                    tot++;
+                }
+                i++;
+            }
+        }
+    }
+}
