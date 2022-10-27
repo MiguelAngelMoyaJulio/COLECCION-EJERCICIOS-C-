41479bf582edfe33584a3ea5323f32fcfdd6df34
MiguelAngelMoyaJulio <mmoyajulio@frba.utn.edu.ar>
2022-10-26 22:54:32 -0300

    E8 FACTORIAL

---
+++ EJERCICIOS/E8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace E8
+{
+    /**
+     * Se ingresa un valor numérico entero, se pide calcular e informar su factorial.
+     */
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = 0,fac=1;
+            
+            Console.WriteLine("ENTER A NUMBER");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                fac = fac * i;
+            }
+
+            Console.WriteLine("FACTORIAL OF "+n+" IS "+fac);            
+        }
+    }
+}
